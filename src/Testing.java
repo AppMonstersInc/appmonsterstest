49d64ada4f4d2de51dc90f3807d2f6e5822c1b9d
Moe 97 <hmohammad75@yahoo.com>
2019-11-25 15:48:06 -0800

    MohammadCode

--- src/Testing.java
+++ src/Testing.java
@@ -1,5 +1,12 @@
 public class Testing {
     public static void main(String[] args) {
         System.out.println("this is a test");
+        System.out.println("Hello World");
+
+        for (;;){
+            System.out.println("Hi");
+        }
+
+
     }
 }
